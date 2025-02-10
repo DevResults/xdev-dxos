89f503629c93ac6551d1fc6373c02f62a5fd7121
Nathan Gerhart <nathan@devresults.com>
2025-02-10 11:16:07 -0700

    hook up "log in"
    once you give yourself a display name, you see the welcome screen

--- app/routes/_private+/_private.tsx
+++ app/routes/_private+/_private.tsx
@@ -1,6 +1,24 @@
+import { useSpaces, useQuery } from "@dxos/react-client/echo";
 import { useIdentity } from "@dxos/react-client/halo";
 import { useEffect } from "react";
 import { Outlet, useNavigate } from "react-router";
+
+const Component = () => {
+  // Get the user to log in before a space can be obtained.
+  const identity = useIdentity();
+  // Get the first available space, created with the identity.
+  const [space] = useSpaces();
+  // Grab everything in the space.
+  const objects = useQuery(space, {});
+  // Show the id of the first object returned.
+  return (
+    <>
+      {JSON.stringify(identity)}
+      {JSON.stringify(space)}
+      {objects.map((d) => JSON.stringify(d))}
+    </>
+  );
+};
 
 export default function Private() {
   const identity = useIdentity();
@@ -8,5 +26,10 @@ export default function Private() {
   useEffect(() => {
     if (!identity?.profile) navigate("/auth/begin");
   }, [identity]);
-  return <Outlet></Outlet>;
+  return (
+    <>
+      <Component />
+      <Outlet />
+    </>
+  );
 }
--- app/routes/auth+/begin.tsx
+++ app/routes/auth+/begin.tsx
@@ -1,3 +1,21 @@
+import { useClient } from "@dxos/react-client";
+import { useIdentity } from "@dxos/react-client/halo";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+
 export default function Begin() {
-  return <>This is where the shell will go</>;
+  const identity = useIdentity();
+  const client = useClient();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (identity?.profile?.displayName) {
+      navigate("/welcome");
+    } else {
+      (async () => {
+        await client.shell.open();
+      })();
+    }
+  }, [identity]);
+  return <></>;
 }
