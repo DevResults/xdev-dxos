e2eec32336e48b9c677d60810afbb28a1584691f
Nathan Gerhart <nathan@devresults.com>
2025-02-10 16:19:08 -0700

    hook up signout
    pretty bumpy, but it works

---
+++ app/routes/auth+/hooks/useSignOut.tsx
@@ -0,0 +1,13 @@
+import { useClient } from "@dxos/react-client";
+import { useLocalState } from "~/hooks/useLocalState";
+
+export const useSignOut = () => {
+  const localState = useLocalState();
+  const client = useClient();
+
+  return async () => {
+    // clear user, device, team from local storage / halo
+    localState.reset();
+    await client.reset();
+  };
+};
