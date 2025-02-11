fb1c3ee6c265bd86ccbadadc56c501c736894045
Nathan Gerhart <nathan@devresults.com>
2025-02-10 22:58:45 -0700

    actually list members

--- app/hooks/useTeam.tsx
+++ app/hooks/useTeam.tsx
@@ -1,13 +1,18 @@
 import { useIdentity } from "@dxos/react-client/halo";
+import { useLocalState } from "./useLocalState";
+import { useMembers, useSpace } from "@dxos/react-client/echo";
 
 /**
  * Takes our auth state and builds a bunch of useful derived state about the user and team.
  */
 export const useTeam = () => {
-  const identity = useIdentity();
+  const identity = { identityKey: "uh oh", profile: { displayName: "help" } }; //useIdentity();
+  const { spaceKey } = useLocalState();
+  const space = useSpace(spaceKey);
+  const members = useMembers(space?.key);
 
   return {
     self: identity,
-    contacts: [identity],
+    contacts: members.map(({ identity }) => identity),
   };
 };
