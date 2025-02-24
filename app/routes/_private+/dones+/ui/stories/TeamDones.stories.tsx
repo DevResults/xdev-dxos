9d593dae57176de7fc40255ac705ea8e5d56a085
Nathan Gerhart <nathan@devresults.com>
2025-02-24 14:10:00 -0700

    hook up team dones story

---
+++ app/routes/_private+/dones+/ui/stories/TeamDones.stories.tsx
@@ -0,0 +1,51 @@
+import { makeRandom } from "@herbcaudill/random"
+import { LocalDate } from "@js-joda/core"
+import type { Meta, StoryObj } from "@storybook/react"
+import { contacts } from "data/contacts"
+import { generateDones } from "routes/_private+/devtools+/danger+/lib/generateDones"
+import type { Contact } from "schema/Contact"
+import { storyContact } from "ui/stories/util/storyContact"
+import { TeamDones } from "../TeamDones"
+import type { DoneEntry } from "~/schema/DoneEntry"
+
+const random = makeRandom("dones")
+
+const meta: Meta<typeof TeamDones> = {
+  title: "Dones/TeamDones",
+  component: TeamDones,
+  args: {
+    contacts,
+    self: storyContact("herb"),
+  },
+}
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+const generateDonesForContacts = (contacts: Contact[]) => {
+  return generateDones({
+    today: LocalDate.parse("2024-11-08"),
+    weeks: 1,
+    productivity: 0.75,
+    enthusiasm: 0.05,
+    contacts,
+  })
+}
+
+export const Empty: Story = {
+  args: {
+    dones: [],
+  },
+}
+
+export const Partial: Story = {
+  args: {
+    dones: generateDonesForContacts(random.sample(contacts, 5)) as DoneEntry[],
+  },
+}
+
+export const Full: Story = {
+  args: {
+    dones: generateDonesForContacts(contacts) as DoneEntry[],
+  },
+}
